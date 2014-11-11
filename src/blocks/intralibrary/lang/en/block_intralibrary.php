b9a57623d8e33d3a6772c0deb37279da2c73fe07
Janek Lasocki-Biczysko <j.lasocki-biczysko@intrallect.com>
2014-11-11 09:39:35 +0000

    Updated block plugin transation file to comply with moodle plugin directory validation

--- src/blocks/intralibrary/lang/en/block_intralibrary.php
+++ src/blocks/intralibrary/lang/en/block_intralibrary.php
@@ -3,12 +3,13 @@ if (!isset($string)) {
     $string = array();
 }
 
+$string['pluginname'] = 'intralibrary Block';  // required for moodle plugin directory validation
+
 $string = array_merge($string,
-        array(
-    'pluginname' => 'Intralibrary Block',
-    'intralibrary:addinstance' => 'Add the intralibrary custom upload',
-    'intralibrary:addmyinstance' => 'Add the intralibray plugin to my moodle page',
-    'uploadto' => "File upload to",
-    'optional_title' => 'Block Title',
-    'optional_body' => 'Block Body'
-));
+    array(
+        'intralibrary:addinstance' => 'Add the intralibrary custom upload',
+        'intralibrary:addmyinstance' => 'Add the intralibray plugin to my moodle page',
+        'uploadto' => "File upload to",
+        'optional_title' => 'Block Title',
+        'optional_body' => 'Block Body'
+    ));

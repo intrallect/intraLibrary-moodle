41b0054afedf416fdbb7d6a7b54c9376c950f3f7
Janek Lasocki-Biczysko <j.lasocki-biczysko@intrallect.com>
2014-12-01 17:06:43 +0000

    bumped version numbers

--- src/filter/intralibrary/version.php
+++ src/filter/intralibrary/version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014102400;
+$plugin->version = 2014120100;
 $plugin->requires = 2014051200;
 $plugin->component = 'filter_intralibrary';
 $plugin->maturity = MATURITY_STABLE;
--- src/repository/intralibrary/version.php
+++ src/repository/intralibrary/version.php
@@ -1,6 +1,6 @@
 <?php
 $plugin->component = "repository_intralibrary";
-$plugin->version = 2014102400;
+$plugin->version = 2014120100;
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0';
